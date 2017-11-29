c3a07fc0566a0a898b006d1a8dde24af08bcea8e
kayhide <kayhide@gmail.com>
2017-11-30 04:06:53 +0900

    update handler to access rds.

--- backend/handler.js
+++ backend/handler.js
@@ -1,16 +1,64 @@
 'use strict';
 
+const co = require('co');
+const promisify = require('util.promisify');
+const AWS = require('aws-sdk');
+const Sequelize = require('sequelize');
+
+const signer = new AWS.RDS.Signer({
+  region: process.env.AWS_RDS_REGION,
+  hostname: process.env.AWS_RDS_HOST,
+  port: parseInt(process.env.AWS_RDS_PORT),
+  username: process.env.AWS_RDS_USER
+});
+
+let token;
+let sequelize;
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  callback(null, response);
+  co(function *() {
+    if (!token) {
+      token = yield promisify(signer.getAuthToken.bind(signer))();
+      const options = {
+        host: process.env.AWS_RDS_HOST,
+        port: parseInt(process.env.AWS_RDS_PORT),
+        ssl: true,
+        dialect: 'mysql',
+        dialectOptions: {
+          ssl: 'Amazon RDS',
+          authSwitchHandler: (data, callback) => {
+            if (data.pluginName === 'mysql_clear_password') {
+              callback(null, Buffer.from(token + '\0'));
+            }
+          }
+        }
+      };
+      sequelize = new Sequelize('mysql', process.env.AWS_RDS_USER, token, options);
+    }
+    const model = sequelize.define('user', {
+      'Host': { type: Sequelize.STRING },
+      'User': { type: Sequelize.STRING, primaryKey: true }
+    }, {
+      tableName: 'user',
+      timestamps: false
+    });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const data = yield model.findAll();
+
+    console.log(data);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Fine!',
+        data: data
+      })
+    };
+
+    callback(null, response);
+  }).catch(err => {
+    token = null;
+    callback(err);
+  });
 };
