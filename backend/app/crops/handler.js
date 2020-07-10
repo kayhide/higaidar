4935f382087daa782800d9267c851ff3e92be3aa
Hideaki Kawai <kayhide@gmail.com>
2020-07-10 22:02:07 +0900

    Create crops handler

---
+++ backend/app/crops/handler.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const _ = require('lodash');
+const co = require('co');
+const util = require('util');
+
+const verify = require('app/verify');
+const parseParams = require('app/parseParams');
+const handleSuccess = require('app/handleSuccess');
+const handleError = require('app/handleError');
+const model = require('app/model');
+
+
+module.exports.index = (event, context, callback) => {
+  // console.log(util.inspect(event, { depth: 5 }));
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  co(function *() {
+    const params = parseParams(event);
+    const { offset, limit } = params.pager({ offset: 0, limit: 50 });
+    const data = yield model.with(m => co(function *() {
+      return m.Crop.findAndCountAll({ order: [['id', 'ASC']], offset, limit });
+    }));
+
+    const items = data.rows.map(item => item.dataValues);
+    handleSuccess(callback)(items, { offset, limit, total: data.count });
+
+  }).catch(handleError(callback));
+};
+
+module.exports.create = (event, context, callback) => {
+  // console.log(util.inspect(event, { depth: 5 }));
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  co(function *() {
+    const params = JSON.parse(event.body);
+    const data = yield model.with(m => {
+      return m.Crop.create(params);
+    });
+
+    handleSuccess(callback)(data.dataValues);
+
+  }).catch(handleError(callback));
+};
+
+module.exports.destroy = (event, context, callback) => {
+  // console.log(util.inspect(event, { depth: 5 }));
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  co(function *() {
+    const id = event.pathParameters.id;
+    const data = yield model.with(m => {
+      return m.Crop.findById(id).then(verify.presence).then(u => u.destroy());
+    });
+
+    handleSuccess(callback)(null);
+
+  }).catch(handleError(callback));
+};
